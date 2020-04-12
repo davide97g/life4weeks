cd5b05a30ecaf215ac0ef2f517c6b4e6ba26bdd2
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2020-04-12 20:23:05 +0200

    test removed function and separated workflows

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,10 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// export const helloWorld = functions.https.onRequest((request, response) => {
+// 	response.send('Hello from Firebase!');
+// });
+
+export const testFunction = functions.https.onRequest((request, response) => {
+	response.send('Hello from Firebase!');
 });
