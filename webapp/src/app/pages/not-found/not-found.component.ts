30abbac0bffd6f151760032ce893c021ec253eba
Davide <fs>
2020-04-30 17:06:28 +0200

    created

--- webapp/src/app/pages/not-found/not-found.component.ts
+++ webapp/src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.sass']
+	selector: 'app-not-found',
+	templateUrl: './not-found.component.html',
+	styleUrls: ['./not-found.component.sass'],
 })
 export class NotFoundComponent implements OnInit {
+	constructor(private router: Router) {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	goToHomepage() {
+		this.router.navigateByUrl('home');
+	}
 }
