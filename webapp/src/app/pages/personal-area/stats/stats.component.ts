bef3c056174a7d0a61f0cc9bcc5a9e52bbd627b9
Davide <fs>
2020-05-06 18:47:15 +0200

    fixed bug with empty records

--- webapp/src/app/pages/personal-area/stats/stats.component.ts
+++ webapp/src/app/pages/personal-area/stats/stats.component.ts
@@ -14,12 +14,13 @@ import { User } from '@models/user';
 	styleUrls: ['./stats.component.sass'],
 })
 export class StatsComponent implements OnInit, AfterViewInit {
-	user: User;
+	user: User = null;
 	records: Record[] = [];
+	emotions: Emotion[] = EmotionList;
 	recordsSinceStartingDate: Record[] = []; // ? records since startingDate
-	startingDate: Date; // ? read from database
-	totalDays: number; // ? difference (in days) between starting date and current date
-	percentage: number; // ? recorded / totalDays
+	startingDate: Date = null; // ? read from database
+	totalDays: number = 0; // ? difference (in days) between starting date and current date
+	percentage: number = 0; // ? recorded / totalDays
 	infoData: InfoData = {
 		title: 'Our statistics',
 		messages: [
@@ -28,26 +29,101 @@ export class StatsComponent implements OnInit, AfterViewInit {
 			'Days recorded : how many days recorded of the total since you registered, in percentage',
 		],
 	};
-	emotions: Emotion[] = EmotionList;
-	@ViewChild('polarArea') polarArea: ElementRef;
+	@ViewChild('graph') graph: ElementRef;
 	graphTypes: string[] = ['doughnut', 'polarArea'];
 	type: string = this.graphTypes[0];
-	chart: Chart;
+	chart: Chart = null;
+	canvas: any = null;
+	ctx: any = null;
+	data: number[] = [];
+	labels: string[] = [];
+	backgroundColors: string[] = [];
+
 	constructor(public auth: AuthService, public router: Router, private dialog: MatDialog) {}
 
 	ngOnInit(): void {
-		// always end first
+		console.info('ngOnInit');
 		this.auth.user$.subscribe((user: User) => {
 			this.user = user;
 		});
 		this.auth.records$.subscribe((records: Record[]) => {
 			this.records = records;
 			this.initUserData();
-			this.updateChartData();
+			this.updateChart();
 		});
 	}
 
-	initUserData() {
+	ngAfterViewInit(): void {
+		console.info('ngAfterViewInit');
+		this.canvas = document.createElement('CANVAS');
+		this.ctx = this.canvas.getContext('2d');
+		this.graph.nativeElement.appendChild(this.canvas);
+		this.updateChart();
+	}
+
+	updateBackgroundColors(): void {
+		console.info('updateBackgroundColors');
+		this.backgroundColors = [];
+		this.emotions.forEach((emotion: Emotion) =>
+			this.backgroundColors.push(emotion.color.rgba.value)
+		);
+	}
+
+	updateLabels(): void {
+		console.info('updateLabels');
+		this.labels = [];
+		this.emotions.forEach((emotion: Emotion) =>
+			this.labels.push(emotion.text + ' ' + emotion.emoji)
+		);
+	}
+
+	updateData(): void {
+		console.info('updateData');
+		this.data = [];
+		this.emotions.forEach((emotion: Emotion) => {
+			this.data.push(this.getDaysEmotion(emotion, this.recordsSinceStartingDate));
+		});
+	}
+
+	/**
+	 * @description creates the chart with the input parameters
+	 */
+	newChart(
+		ctx: any,
+		type: string,
+		data: number[],
+		labels: string[],
+		backgroundColors: string[],
+		title?: string
+	): Chart {
+		console.info('newChart');
+		let display: boolean = this.recordsSinceStartingDate.length == 0 ? false : true;
+		return new Chart(ctx, {
+			type: type,
+			data: {
+				labels: labels,
+				datasets: [
+					{
+						label: title ? title : 'Days for Emotions',
+						data: data,
+						backgroundColor: backgroundColors,
+					},
+				],
+			},
+			options: {
+				legend: {
+					display: display,
+					position: 'right',
+				},
+			},
+		});
+	}
+
+	/**
+	 * @description fill the statistics from the user's data
+	 */
+	initUserData(): void {
+		console.info('initUserData');
 		this.startingDate = new Date(this.user.metadata.creationTime);
 		this.totalDays =
 			Math.floor((new Date().getTime() - this.startingDate.getTime()) / (1000 * 3600 * 24)) +
@@ -58,57 +134,52 @@ export class StatsComponent implements OnInit, AfterViewInit {
 		this.percentage = Math.round((this.recordsSinceStartingDate.length / this.totalDays) * 100);
 	}
 
-	ngAfterViewInit(): void {
-		let ctx = this.polarArea.nativeElement.getContext('2d');
-		let backgroundColors: string[] = [];
-		let labels: string[] = [];
-		let data: number[] = [];
-		this.emotions.forEach((emotion: Emotion) => {
-			backgroundColors.push(emotion.color.rgba.value);
-			labels.push(emotion.text + ' ' + emotion.emoji);
-			data.push(this.getDaysEmotion(emotion));
-		});
-		this.chart = new Chart(ctx, {
-			type: this.type,
-			data: {
-				labels: labels,
-				datasets: [
-					{
-						label: 'Days for Emotions',
-						data: data,
-						backgroundColor: backgroundColors,
-					},
-				],
-			},
-			options: {
-				legend: {
-					position: 'right',
-				},
-			},
-		});
+	/**
+	 * @description updates the chart with the data from records
+	 */
+	updateChart(): void {
+		console.info('updateChartData');
+		// this.chart.data.datasets[0].data = this.getData();
+		// this.chart.update({ duration: 2000 });
+		this.updateBackgroundColors();
+		this.updateLabels();
+		this.updateData();
+
+		this.chart = this.newChart(
+			this.ctx,
+			this.type,
+			this.data,
+			this.labels,
+			this.backgroundColors
+		);
 	}
 
-	updateChartData(): void {
-		let data: number[] = [];
-		this.emotions.forEach((emotion: Emotion) => data.push(this.getDaysEmotion(emotion)));
-		this.chart.data.datasets[0].data = data;
-		this.chart.update({ duration: 2000 });
-	}
-
-	getDaysEmotion(emotion: Emotion): number {
+	/**
+	 *
+	 * @param emotion
+	 */
+	getDaysEmotion(emotion: Emotion, records: Record[]): number {
+		console.info('getDaysEmotion');
 		let tot: number = 0;
-		this.recordsSinceStartingDate.forEach((record: Record) => {
+		records.forEach((record: Record) => {
 			if (record.emotion.text === emotion.text) tot++;
 		});
 		return tot;
 	}
 
+	/**
+	 * @description changes the graph type of the chart
+	 * @param type graph type
+	 */
 	changeType(type: string) {
-		this.chart.config.type = type;
-		this.chart.update({ duration: 2000 });
-		console.info(this.chart.config.type);
+		console.info('changeType', type);
+		this.type = type;
+		this.updateChart();
 	}
 
+	/**
+	 * @description opens the info dialog
+	 */
 	openDialog(): void {
 		this.dialog.open(DialogInfoComponent, {
 			width: '350px',
